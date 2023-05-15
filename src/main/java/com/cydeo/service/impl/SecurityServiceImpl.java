a02c8b48251300942b255f62c70af5dbf7709de2
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-05-15 15:52:51 -0400

    added security service impl

--- src/main/java/com/cydeo/service/impl/SecurityServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/SecurityServiceImpl.java
@@ -1,12 +1,30 @@
 package com.cydeo.service.impl;
 
+import com.cydeo.entity.User;
+import com.cydeo.entity.common.UserPrincipal;
+import com.cydeo.repository.UserRepository;
 import com.cydeo.service.SecurityService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SecurityServiceImpl implements SecurityService {
+
+    private final UserRepository userRepository;
+
+    public SecurityServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+
+        User user = userRepository.findByUserNameAndIsDeleted(username, false);
+
+        if(user == null){
+            throw new UsernameNotFoundException(username);
+        }
+        return new UserPrincipal(user); // get user from db, and convert to user Spring understands by using mapper through UserPrincipal
     }
 }
