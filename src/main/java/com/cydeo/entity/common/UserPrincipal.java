2d9d8707945e711c850d3e63930133a671fe2bb1
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-05-15 15:30:31 -0400

    added impl of methods

--- src/main/java/com/cydeo/entity/common/UserPrincipal.java
+++ src/main/java/com/cydeo/entity/common/UserPrincipal.java
@@ -1,44 +1,58 @@
 package com.cydeo.entity.common;
 
+import com.cydeo.entity.User;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class UserPrincipal implements UserDetails {
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+    private User user; // get user from db and convert to Spring then Spring does automatically
+
+    public UserPrincipal(User user) {
+        this.user = user;
     }
 
     @Override
-    public String getPassword() { // get user password from db, convert to Spring then Spring does automatically
-        return null;
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        List<GrantedAuthority> authorityList = new ArrayList<>();
+        GrantedAuthority authority = new SimpleGrantedAuthority(this.user.getRole().getDescription());
+        authorityList.add(authority);
+        return authorityList;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.user.getPassWord();
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return this.user.getUserName();
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true; // since we do not have any field in the entity
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return this.user.isEnabled();
     }
 }
