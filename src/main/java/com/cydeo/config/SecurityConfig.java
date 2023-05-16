7c8131c111fe68377dd7f93f6a40683537ed0854
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-05-16 13:24:23 -0400

    updated

--- src/main/java/com/cydeo/config/SecurityConfig.java
+++ src/main/java/com/cydeo/config/SecurityConfig.java
@@ -69,6 +69,7 @@ public class SecurityConfig {
                 .logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 .logoutSuccessUrl("/login")
-                .and().build();
+                .and()
+                .build();
     }
 }
